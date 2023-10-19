d20fc6e58b3b36781dcbe666b2b875df366b7a36
Tiago Leao <tiago.leao@instanda.com>
2023-10-19 14:45:22 +0100

    Revert "Revert "Revert "Revert "asdfghj""""
    This reverts commit a5f0b9fe0fbd1bc3a7bca9c462382031b8765027.

--- AnyStore/Helpers/CalculationHelper.cs
+++ AnyStore/Helpers/CalculationHelper.cs
@@ -52,6 +52,11 @@ namespace AnyStore.DAL
             }
 
             dt = new DataTable();
+            dt = new DataTable();
+            dt = new DataTable();
+            dt = new DataTable();
+            dt = new DataTable();
+            dt = new DataTable();
 
             return dt;
         }
